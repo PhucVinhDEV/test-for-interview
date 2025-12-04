75ad672d4219e0ab0c33c8559be7e61c9133965f
oIoVinhGa <tpvinh292931@gmail.com>
2025-12-04 15:01:17 +0700

    Initial commit

---
+++ src/test/java/com/example/test_for_interview/TestForInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_for_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
